c513066be6f27699492cabf61626c2decd08c1f1
bimarquispeUAB <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>
2025-10-13 22:05:43 -0400

    style: aplica CSS mínimo y responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,6 @@
 import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
-import { HomeComponent } from './pages/home/home.component';
-import { ServiciosComponent } from './pages/servicios/servicios.component';
-import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'servicios', component: ServiciosComponent },
-  { path: 'contacto', component: ContactoComponent }
-];
 export const routes: Routes = [
   {
     path: '',
@@ -17,17 +9,17 @@ export const routes: Routes = [
       {
         path: '',
         loadComponent: () =>
-          import('./pages/home/home.component').then(m => m.HomeComponent)
+          import('./pages/home/home').then(m => m.Home)
       },
       {
         path: 'servicios',
         loadComponent: () =>
-          import('./pages/servicios/servicios.component').then(m => m.ServiciosComponent)
+          import('./pages/servicios/servicios').then(m => m.Servicios)
       },
       {
         path: 'contacto',
         loadComponent: () =>
-          import('./pages/contacto/contacto.component').then(m => m.ContactoComponent)
+          import('./pages/contacto/contacto').then(m => m.Contacto)
       }
     ]
   }
