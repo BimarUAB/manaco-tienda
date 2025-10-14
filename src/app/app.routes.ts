fdf21bc19ed94fedf43b0d5d033856358cf77dae
bimarquispeUAB <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>
2025-10-13 21:18:47 -0400

    feat(router): configura rutas basicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { HomeComponent } from './pages/home/home.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component').then(m => m.HomeComponent)
+      },
+      {
+        path: 'servicios',
+        loadComponent: () =>
+          import('./pages/servicios/servicios.component').then(m => m.ServiciosComponent)
+      },
+      {
+        path: 'contacto',
+        loadComponent: () =>
+          import('./pages/contacto/contacto.component').then(m => m.ContactoComponent)
+      }
+    ]
+  }
+];
